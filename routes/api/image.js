0d8da8bd7c241767ab90f6ff2d319610e3db1555
hungcutephomaiqueconbocuoi <hung0868358175@gmail.com>
2024-01-07 22:08:28 +0700

    fix bug

--- routes/api/image.js
+++ routes/api/image.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const ImageController = require('../../controller/ImageControlloer')
+const {ImageController} = require('../../controller/ImageControlloer')
 
 router
     .delete('/', ImageController.removeImage)
