adf6558fa8c08f6b17c5586bd2783d7999000c0d
hungcutephomaiqueconbocuoi <hung0868358175@gmail.com>
2024-01-15 05:27:45 +0700

    fix authentication for admin page

--- routes/view/_offer.js
+++ routes/view/_offer.js
@@ -1,17 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/offer-list', function(req, res) {
+router.get('/offer-list', function (req, res) {
+  console.log(req.cookies.login)
+  if (req.cookies.login == null) {
+    res.send('Bạn cần có quyền để tiếp cận trang này!')
+    return
+  }
   res.render('offer/offer_list')
 })
 
-router.get('/offer-create', function(req, res) {
+router.get('/offer-create', function (req, res) {
+  if (req.cookies.login == null) {
+    res.send('Bạn cần có quyền để tiếp cận trang này!')
+    return
+  }
   res.render('offer/offer_create');
 });
 
-router.get('/offer-detail/:id', function(req, res) {
+router.get('/offer-detail/:id', function (req, res) {
+  if (req.cookies.login == null) {
+    res.send('Bạn cần có quyền để tiếp cận trang này!')
+    return
+  }
   const id = req.params.id
-  res.render('offer/offer_detail', {id: id});
+  res.render('offer/offer_detail', { id: id });
 });
 
 module.exports = router;
