fcf7a081cfec2f8e219983a9e0e1113bef4d9293
hungcutephomaiqueconbocuoi <hung0868358175@gmail.com>
2024-01-17 22:06:49 +0700

    fix notification list issue

--- routes/api/notification.js
+++ routes/api/notification.js
@@ -2,10 +2,14 @@ var express = require('express');
 var router = express.Router();
 const NotificationController = require('../../controller/NotificationController')
 
+const { Constraint } = require('../../utlity')
+const passport = require('passport')
+require('../../middleware/passport')
+
 router
     .post('/', NotificationController.pushNoti)
     .get('/',NotificationController.get)
-    .get('/get-by-id-user/:id_user',NotificationController.getByIdUser)
+    .get('/get-by-id-user/', passport.authenticate(Constraint.JWT, { session: false }, null), NotificationController.getByIdUser)
     .get('/get-notification-offer',NotificationController.getNotificationOffer)
     .put('/:idN',NotificationController.updateSeenNotification)
     .get('/get-quantity-notification/:idU',NotificationController.getQuantityNotification)
